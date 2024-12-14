1cd9d396f6663d31b983d6a165545c5dfd68fb91
Yuvraj Gajanan Budhwat <yuvrajbudhwat@gmail.com>
2024-12-14 21:54:43 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+package encapsulation.example1;
+
+public class Area {
+	int length;
+	int breadth;
+	
+	Area(int length, int breadth){
+		this.length=length;
+		this.breadth=breadth;
+	}
+	
+	public void getArea() {
+		int area=length*breadth;
+		System.out.println("Area is: "+ area);
+	}
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+package encapsulation.example1;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Area a1=new Area(3,4);
+		a1.getArea();
+	}
+}
